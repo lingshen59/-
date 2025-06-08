451da6d9029c5042bbb40f353b4c41bb70142898
lingshen59 <miguel784590@gmail.com>
2025-06-08 15:52:58 -0400

    Delete form_web directory

--- form_web/static/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.onkeydown = function(e) {
-    if (e.keyCode == 123 || 
-        (e.ctrlKey && e.shiftKey && ['I','C','J'].includes(e.key.toUpperCase())) || 
-        (e.ctrlKey && e.key === 'u')) {
-        return false;
-    }
-};
